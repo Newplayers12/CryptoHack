8867e7cc7d1df09b588d375cccf831e5441a31f6
Nhan Gia Khâm <nhangiakham@gmail.com>
2023-04-28 23:04:01 +0700

    changed sol

--- One_Time_Pad/Gotta_Go_Fast/sol.py
+++ One_Time_Pad/Gotta_Go_Fast/sol.py
@@ -4,13 +4,6 @@ import json
 r = remote('socket.cryptohack.org', 13372)
 print(r.recv())
 
-
-send_flag = {
-    "option": "get_flag"
-}
-
-# idea was the current time what taking int(time.time()) which was taking the number of second
-# Therefore just be quick enough for it not to change -> two time pad
 def json_recv():
     line = r.recv()
     print(line)
@@ -22,15 +15,8 @@ def json_send(hsh):
     
 json_send(send_flag)
 data = json_recv()
-
 flag = bytes.fromhex(data['encrypted_flag'])
 
-print(flag)
-
-send_data = {
-    "option": "encrypt_data",
-    "input_data": "ff"*len(flag)
-}
 
 json_send(send_data)
 data = json_recv()
@@ -41,7 +27,7 @@ pad = bytes.fromhex("ff"*len(flag))
 
 r.close()
 
-print(xor(flag, pad, temp))
+print(flag * pad * temp)
 
 
 
