fab5e782715aac13e0f1b3fd32d042f7d988ed91
Nhan Gia Khâm <nhangiakham@gmail.com>
2023-05-01 13:21:09 +0700

    update

--- Mathematics/Lattices/Gaussian_Reduction.py
+++ Mathematics/Lattices/Gaussian_Reduction.py
@@ -1,2 +1,17 @@
 from sage.all import *
 
+v = vector((846835985, 9834798552))
+u = vector((87502093, 123094980))
+
+
+def Gaussian_Reduction(u, v):
+    while True:
+        if v.norm() < u.norm(): u, v = v, u
+        m = round(u * v / (u * u))
+        if m == 0: return u, v
+        v = v - m * u
+    pass
+
+p, q = Gaussian_Reduction(u, v)
+
+print(p * q)
