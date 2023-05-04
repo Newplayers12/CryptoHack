c76fddb115f251fae41eabb1851d2d5db126ac35
Nhan Gia Khâm <nhangiakham@gmail.com>
2023-05-04 20:18:43 +0700

    update

--- One_Time_Pad/No_Leaks/sol.py
+++ One_Time_Pad/No_Leaks/sol.py
@@ -1,6 +1,7 @@
 from pwn import * 
 import json
 import time
+import base64
 
 r = remote('socket.cryptohack.org', 13370)
 print(r.recv())
@@ -13,18 +14,24 @@ def json_send(hsh):
     request = json.dumps(hsh).encode()
     r.sendline(request)
 
+
 while True:    
-    start = time.time()
+    
     json_send({"msg": "request"})
     data = json_recv()
-    
-    print(time.time() - start)
+        
     if 'ciphertext' in data:
-        print(data['ciphertext'])
+        b = base64.b64decode(data['ciphertext'])
+        """
+            MAGIC
+        """
     
     
-
-
+    """
+        MAGIC
+    """
+    
+# flag = b'c'b'r'b'y'b'p'b't'b'o'b'{'b'u'b'n'b'r'b'4'b'n'b'd'b'0'b'm'b'_'b'0'b'7'b'p'b'}'
 
 
 r.close()
